1c268c6a3467545f24a94816473df92696f057bb
Saki <amethyst225crystal@gmail.com>
2025-10-27 06:29:46 +0900

    database.ts変更

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URI || "");
+    console.log("MongoDB 接続成功");
   } catch (error) {
-    console.log(error + "DB接続に失敗しました。");
-    throw new Error();
+    console.error("DB接続に失敗しました:", error);
+    throw error;
   }
 };
